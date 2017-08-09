2f7207d24a6044bb7182cf9a8c96ed9b08e41a9e
Aaron Hosford <aaron.hosford@ericsson.com>
2017-08-09 18:18:55 -0500

    Debugging

--- aiml_sets/__init__.py
+++ aiml_sets/__init__.py
@@ -6,7 +6,8 @@ import shutil
 __author__ = 'Aaron Hosford'
 
 
-_ROOT_PATH = os.path.dirname(__file__)
+_ROOT_PATH = os.path.abspath(os.path.dirname(__file__))
+_DEFAULT_INSTALL_PATH = os.path.abspath(os.path.expanduser('~/.aiml'))
 
 
 def list_aiml_sets() -> list:
@@ -18,10 +19,9 @@ def has_aiml_set(set_name: str):
 
 
 def get_aiml_set_path(set_name: str) -> str:
-    set_path = os.path.join(_ROOT_PATH, set_name)
-    if not os.path.isdir(set_path) or set_name[:1].isalpha():
+    if not has_aiml_set(set_name):
         raise KeyError(set_name)
-    return set_path
+    return os.path.join(_ROOT_PATH, set_name)
 
 
 def list_aiml_files(set_name: str = None, pattern: str = None) -> list:
@@ -36,7 +36,7 @@ def list_aiml_files(set_name: str = None, pattern: str = None) -> list:
         return glob.glob(os.path.join(set_path, pattern))
 
 
-def install(destination_path=os.path.expanduser('~/.aiml'), set_name: str = None, pattern: str = None) -> str:
+def install(set_name: str = None, pattern: str = None, destination_path: str = _DEFAULT_INSTALL_PATH) -> str:
     if set_name is None:
         set_names = list_aiml_sets()
     else:
@@ -44,9 +44,25 @@ def install(destination_path=os.path.expanduser('~/.aiml'), set_name: str = None
         if not has_aiml_set(set_name):
             raise KeyError(set_name)
     for set_name in set_names:
-        install_path = os.path.join(destination_path, set_name)
-        if not os.path.isdir(install_path):
-            os.makedirs(install_path)
         for aiml_file in list_aiml_files(set_name, pattern):
-            shutil.copy2(aiml_file, os.path.join(install_path, aiml_file))
+            path_tail = aiml_file[len(_ROOT_PATH):].lstrip('/').lstrip('\\')
+            copy_path = os.path.join(destination_path, path_tail)
+            if not os.path.isdir(os.path.dirname(copy_path)):
+                os.makedirs(os.path.dirname(copy_path))
+            shutil.copy2(aiml_file, copy_path)
     return destination_path
+
+
+def is_installed(set_name: str = None, pattern: str = None, destination_path: str = _DEFAULT_INSTALL_PATH) -> bool:
+    if set_name is None:
+        set_names = list_aiml_sets()
+    else:
+        set_names = [set_name]
+        if not has_aiml_set(set_name):
+            raise KeyError(set_name)
+    for set_name in set_names:
+        for aiml_file in list_aiml_files(set_name, pattern):
+            path_tail = aiml_file[len(_ROOT_PATH):].lstrip('/').lstrip('\\')
+            if not os.path.isfile(os.path.join(destination_path, path_tail)):
+                return False
+    return True
